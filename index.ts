19bf059561443b8b3893f6d24ae89877d5fc15f8
chenelias <eliaschen.dev@gmail.com>
2024-11-10 20:30:23 +0800

    add muti model

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import OpenAI from "openai";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import Anthropic from "@anthropic-ai/sdk";
 
 console.log("🚩 Service is starting...");
 
@@ -9,32 +10,47 @@ const openai = new OpenAI({
 });
 const app = new Hono();
 
-app.get("/gemini", async (c) => {
-  console.log("⚡️ Gemini endpoint hit ⚡️");
+app.post("/:model", async (c) => {
+  const id = c.req.param("model");
+  let result = null;
+  let runTime;
+  const _start_ = performance.now();
+  switch (id) {
+    case "gemini":
+      console.log("⚡️ Gemini endpoint hit ⚡️");
+      const body = await c.req.text();
+      const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const prompt = "summary the html content: " + body;
+      result = await model.generateContent(prompt);
+      // console.log("🚀 Result:", result.response.text());
+      break;
+    case "claude":
+      const anthropic = new Anthropic({
+        apiKey: process.env.ANTHROPIC_API_KEY,
+      });
 
-  const body = await c.req.text();
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  console.log(body);
-  const prompt = "summary the html content: " + body;
-
-  const result = await model.generateContent(prompt);
-  console.log("🚀 Result:", result.response.text());
-  return c.json({ message: result.response.text() });
-});
-
-app.get("/gpt", async (c) => {
-  console.log("⚡️ GPT endpoint hit ⚡️");
-  try {
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "write a haiku about ai" }],
-    });
-    return c.json({ message: completion.choices[0].message.content });
-  } catch (error) {
-    console.error("Error fetching completion:", error);
-    return c.json({ error: "Failed to fetch completion" }, 500);
+      const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1024,
+        messages: [{ role: "user", content: "Hello, Claude" }],
+      });
+      console.log("🚀 Result:", msg);
+      result = msg.choices[0].message.content;
+    case "gpt":
+      console.log("⚡️ GPT endpoint hit ⚡️");
+      const completion = await openai.chat.completions.create({
+        model: "o1-mini",
+        messages: [{ role: "user", content: "hello" }],
+      });
+      result = completion.choices[0].message.content;
+      break;
+    default:
+      return c.json({ error: "Model not found" }, 404);
   }
+  const _end_ = performance.now();
+  runTime = ((_end_ - _start_) / 1000).toFixed(2);
+  return c.json({ message: result, time: runTime + "s" });
 });
 
 Bun.serve({
